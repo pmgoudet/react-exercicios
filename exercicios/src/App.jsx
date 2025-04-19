8b5d061723ebbb9457e2640efd643e6a53a2ce23
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-04-19 11:08:12 +0200

    f
    f

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -1,4 +1,3 @@
-
 //! EXERCICIO 1 /////////////////////////////////////////////////////////
 
 // import { useState } from 'react'
@@ -33,8 +32,6 @@
 //       setNumero(0);
 //   }
 
-
-
 //   return (
 //     <div>
 //       <p>{numero}</p>
@@ -47,10 +44,7 @@
 
 // export default App
 
-
-
 //! EXERCICIO 2 /////////////////////////////////////////////////////////
-
 
 // import { useState } from 'react'
 // import './App.css'
@@ -73,9 +67,7 @@
 
 // export default App
 
-
 //! EXERCICIO 3 /////////////////////////////////////////////////////////
-
 
 // import { useState } from 'react'
 // import './App.css'
@@ -98,11 +90,7 @@
 
 // export default App
 
-
-
-
 //! EXERCICIO 4 /////////////////////////////////////////////////////////
-
 
 // import { useState } from 'react'
 // import './App.css'
@@ -125,48 +113,180 @@
 
 // export default App
 
-
-
-
-
 //! EXERCICIO 5 /////////////////////////////////////////////////////////
 
+// import { useState } from 'react'
+// import './App.css'
 
-import { useState } from 'react'
-import './App.css'
+// function App() {
+
+//   const [name, setName] = useState ('')
+//   const [nameFinal, setNameFinal] = useState ('')
+//   const [email, setEmail] = useState ('')
+//   const [emailFinal, setEmailFinal] = useState ('')
+//   const [mostra, setMostra] = useState (false)
+
+//   const pegaNome = (e) => {
+//     setName(e.target.value)
+//   }
+
+//   const pegaEmail = (e) => {
+//     setEmail(e.target.value)
+//   }
+
+//   const sub = (e) => {
+//     e.preventDefault()
+//     name && email ? setMostra(true) : setMostra(false)
+//     setNameFinal(name)
+//     setEmailFinal(email)
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={sub}>
+//         <input type="text" placeholder='name' onChange={pegaNome}/><br />
+//         <input type="text" placeholder='email' onChange={pegaEmail}/><br />
+//         <button type='submit'>Submit</button>
+//       </form>
+//       {mostra && <p>Olá, {nameFinal}! Seu email é {emailFinal}.</p>}
+//     </div>
+//   )
+// }
+
+// export default App
+
+//! EXERCICIO 6 /////////////////////////////////////////////////////////
+
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+
+//   const [task, setTask] = useState('')
+//   const [tasks, setTasks] = useState([])
+
+//   const saveTask = (e) => {
+//     setTask(e.target.value)
+//   }
+
+//   const add = (e) => {
+//     e.preventDefault()
+//     task && setTasks([...tasks, task])
+//     setTask('')
+//     console.log(tasks)
+//   }
+
+//   return (
+//     <div>
+//         <form onSubmit={add}>
+//           <input type="text" placeholder='Crie sua task' onChange={saveTask} value={task}/><br />
+//           <button type='submit'>Adicionar</button>
+//         </form>
+//         <ul>
+//           {tasks.map(task => (
+//             <li>{task}</li>
+//           ))}
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//! EXERCICIO 7 /////////////////////////////////////////////////////////
+
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+
+//   const [valor, setValor] = useState(0)
+//   const [msg, setMsg] = useState('')
+
+//   const soma = () => {
+//     valor<10 ? (setValor(valor+1), setMsg('')) : setMsg('Valor máximo atingido')
+//   }
+
+//   const subtrai = () => {
+//     valor>0 ? (setValor(valor-1), setMsg('')) : setMsg('Valor mínimo atingido')
+//   }
+
+//   return (
+//     <div>
+//       <p>{valor}</p>
+//       <button onClick={subtrai}>-</button>
+//       <button onClick={soma}>+</button>
+//       <p>{msg}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//! EXERCICIO 8 /////////////////////////////////////////////////////////
+
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+
+//   const [msg, setMsg] = useState('Olá!')
+
+//   const troca = () => {
+//     msg === 'Olá!'? setMsg('Tchau') : setMsg('Olá!')
+//   }
+
+//   return (
+//     <div>
+//       <p>{msg}</p>
+//       <button onClick={troca}>Trocar mensagem</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//todo AQUI AUMENTA A DIFICULDADE
+
+//! EXERCICIO 9 /////////////////////////////////////////////////////////
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [mostra, setMostra] = useState(false)
-
-  const defineNome = (e) => {
-    setNome(e.target.value)
-  }
-
-  const defineEmail = (e) => {
-    setEmail(e.target.value)
-  }
-
-  const sub = (event) => {
-    event.preventDefault()
-    setMostra(!mostra)
-
-    setNome('')
-    setEmail('')
-  }
-
+  const [display, setDisplay] = useState("");
+  const [operacao, setOperacao] = useState("");
 
   return (
-    <form onSubmit={sub}>
-      <input type="text" placeholder='nome' onChange={defineNome}/><br />
-      <input type="text" placeholder='email' onChange={defineEmail}/> <br />
-      {mostra && <button type="submit" >Enviar</button>}
+    <div>
+      <button value={"="}>=</button>
+      <br />
+      <br />
+      <button value={1}>1</button>
+      <button value={2}>2</button>
+      <button value={3}>3</button>
+      <br />
+      <button value={4}>4</button>
+      <button value={5}>5</button>
+      <button value={6}>6</button>
+      <br />
+      <button value={7}>7</button>
+      <button value={8}>8</button>
+      <button value={9}>9</button>
+      <br />
+      <button value={0}>0</button>
+      <br />
+      <br />
+      <button value={"+"}>+</button>
+      <button value={"-"}>-</button>
+      <button value={"*"}>*</button>
+      <button value={"/"}>/</button>
+      <br />
+      <br />
 
-      {mostra && <p>"Olá, {nome}! Seu email é {email}."</p>}
-    </form>
-  )
+      <p>Resultado: </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
